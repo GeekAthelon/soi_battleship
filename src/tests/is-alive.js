5b501bdc0ec358ee3562a8c98ae64cdb7495bf49
Geek Athelon <geekathelon@gmail.com>
2018-11-10 20:13:31 -0500

    Test framework added

---
+++ src/tests/is-alive.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+describe("is alive", () => {
+    it("should be alive", () => {
+        assert.ok(true);
+    });
+});
---
+++ webpack-test.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var nodeExternals = require('webpack-node-externals');
+var WebpackShellPlugin = require('webpack-shell-plugin');
+
+var config = {
+ entry: './all-tests.js',
+ output: {
+   filename: 'testBundle.js'
+},
+ target: 'node',
+ externals: [nodeExternals()],
+ node: {
+   fs: 'empty'
+ },
+ plugins: [
+   new WebpackShellPlugin({
+     //onBuildExit: "node_modules\\.bin\\mocha testBundle.js"
+     onBuildExit: "npm run testbundle"
+   })
+ ]
+};
+
+module.exports = config;
